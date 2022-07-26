69b0ee9be5e6bb1571f3cb3547e22acf29698b28
Nimmymathew <nimmymaths91@gmail.com>
2022-07-26 17:04:04 +0530

    Running local machine suceesfully

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+
+@app.route("/",methods=['GET','POST'])
+def  index():
+   
+    return "This is starting of machine learning project"
+
+
+if __name__== "__main__" :
+       app.run(debug=True)
+
