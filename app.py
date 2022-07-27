e12ec82cc645e3a76cfb59c1fa40a3a3479de1a1
Nimmymathew <nimmymaths91@gmail.com>
2022-07-27 12:42:23 +0530

    log file added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from Bike.logger import logging
 
 app=Flask(__name__)
-
-
 @app.route("/",methods=['GET','POST'])
 def  index():
+    logging.info("we are testing logging")
    
     return "This is starting of machine learning project"
 
